8769923b97e241c6e5de3dbd1eb6ada2523089c6
GitRowin <20298291+GitRowin@users.noreply.github.com>
2023-08-19 19:06:28 +0200

    Add exe extension to Windows executable

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-goos=windows goarch=amd64 go build -ldflags="-s -w" -trimpath -o httping-windows-amd64
+goos=windows goarch=amd64 go build -ldflags="-s -w" -trimpath -o httping-windows-amd64.exe
 goos=linux goarch=amd64 go build -ldflags="-s -w" -trimpath -o httping-linux-amd64
 goos=linux goarch=arm64 go build -ldflags="-s -w" -trimpath -o httping-linux-arm64
